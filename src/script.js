9e2af9d4c5a562bf70448d02a8fc9051d3fc7065
Nikola Trtic <nikolatrtic02@gmail.com>
2025-06-19 14:06:37 +0200

    lazy loading images

--- src/script.js
+++ src/script.js
@@ -169,9 +169,37 @@ function initMobileMenu() {
 }
 
 
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.src = entry.target.dataset.src;
+  console.log(entry.target.dataset.src);
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '300px',
+});
+
+
+
+
+
 //Inicijalizacija 
 function initUI() {
   document.getElementById("year").textContent = new Date().getFullYear();
+  imgTargets.forEach(img => imgObserver.observe(img));
   initSwiper();
   initNavLinks();
   initSectionReveal();
